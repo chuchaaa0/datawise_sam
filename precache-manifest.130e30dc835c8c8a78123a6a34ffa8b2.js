78881ab77874c28a4dfd821c64ff7135d988ee29
mudit-a-HT <mudit.agarwal@hindustantimes.com>
2021-02-09 13:45:40 +0530

    Updates

---
+++ precache-manifest.130e30dc835c8c8a78123a6a34ffa8b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45858d10cf2001870cab64c8f0a8d3e0",
+    "url": "/datawise_sam/index.html"
+  },
+  {
+    "revision": "e1386e99a202a8a16a63",
+    "url": "/datawise_sam/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3b64ed3d8ce57fa72ad0",
+    "url": "/datawise_sam/static/css/main.a8e55fa4.chunk.css"
+  },
+  {
+    "revision": "e1386e99a202a8a16a63",
+    "url": "/datawise_sam/static/js/2.5fae3617.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/datawise_sam/static/js/2.5fae3617.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b64ed3d8ce57fa72ad0",
+    "url": "/datawise_sam/static/js/main.1cbb7603.chunk.js"
+  },
+  {
+    "revision": "c989beab502f2925461e",
+    "url": "/datawise_sam/static/js/runtime-main.0b1b1e49.js"
+  }
+]);
